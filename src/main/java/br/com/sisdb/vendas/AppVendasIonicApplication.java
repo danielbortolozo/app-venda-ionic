b55600cb16816549c89fa98b44c08b6c6381e35c
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2020-03-31 00:06:55 -0300

    first commit

---
+++ src/main/java/br/com/sisdb/vendas/AppVendasIonicApplication.java
@@ -0,0 +1,13 @@
+package br.com.sisdb.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVendasIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVendasIonicApplication.class, args);
+	}
+
+}

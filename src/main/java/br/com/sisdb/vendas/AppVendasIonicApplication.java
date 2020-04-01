96245ccd59fb46c4842543b033c19210abbefdb4
Daniel R. Bortolozo <danielbortolozo@hotmail.com>
2020-03-31 22:01:10 -0300

    Adicionando cidade e estados

--- src/main/java/br/com/sisdb/vendas/AppVendasIonicApplication.java
+++ src/main/java/br/com/sisdb/vendas/AppVendasIonicApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.sisdb.vendas.domains.Categoria;
+import br.com.sisdb.vendas.domains.Cidade;
+import br.com.sisdb.vendas.domains.Estado;
 import br.com.sisdb.vendas.domains.Produto;
 import br.com.sisdb.vendas.repositories.CategoriaRepository;
+import br.com.sisdb.vendas.repositories.CidadeRepository;
+import br.com.sisdb.vendas.repositories.EstadoRepository;
 import br.com.sisdb.vendas.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class AppVendasIonicApplication  implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository prodRepos;
+	
+	@Autowired
+	private CidadeRepository cidadeRepos;
+	
+	@Autowired
+	private EstadoRepository estadoRepos;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AppVendasIonicApplication.class, args);
@@ -41,9 +51,24 @@ public class AppVendasIonicApplication  implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(c1, c2));
 		p3.getCategorias().addAll(Arrays.asList(c1));
 		
-		
 		catRepos.saveAll(Arrays.asList(c1,c2));
 		prodRepos.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Fernandopolis", est2);
+		Cidade cid2 = new Cidade(null, "Iturama", est1);
+		Cidade cid3 = new Cidade(null, "uberlandia", est1);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		
+		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
+		estadoRepos.saveAll(Arrays.asList(est1, est2));
+		
+		cidadeRepos.saveAll(Arrays.asList(cid1, cid2, cid3));
+		
 	}
 
 }

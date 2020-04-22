7096a1508a2e286f6730a04bed2e67b53f178573
Daniel R. Bortolozo <danielbortolozo@hotmail.com>
2020-04-21 21:23:08 -0300

    Esqueci a senha

--- src/main/java/br/com/sisdb/vendas/services/EmailService.java
+++ src/main/java/br/com/sisdb/vendas/services/EmailService.java
@@ -3,6 +3,8 @@ package br.com.sisdb.vendas.services;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
+
+import br.com.sisdb.vendas.domains.Cliente;
 import br.com.sisdb.vendas.domains.Pedido;
 
 public interface EmailService {
@@ -13,5 +15,7 @@ public interface EmailService {
 	
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	void sendHtmlEmail(MimeMessage mm);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
